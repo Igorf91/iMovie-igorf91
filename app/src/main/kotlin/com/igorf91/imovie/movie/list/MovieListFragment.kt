fb51350c8bbf1b1281924fee76c272c99a600333
igor <igor.fds91@hotmail.com>
2019-11-16 16:00:42 -0300

    Move to kotlin package; Create viewModel;

--- app/src/main/kotlin/com/igorf91/imovie/movie/list/MovieListFragment.kt
+++ app/src/main/kotlin/com/igorf91/imovie/movie/list/MovieListFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.igorf91.imovie.R
 import com.igorf91.imovie.adapter.MediaListAdapter
@@ -12,10 +14,17 @@ import kotlinx.android.synthetic.main.fragment_movie_list.movies_list_recycler_v
 
 class MovieListFragment : Fragment() {
 
+    private val adapter = MediaListAdapter()
+    private lateinit var movieListViewModel: MovieListViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        movieListViewModel = ViewModelProviders
+            .of(this, MovieListViewModelFactory(MovieListRepository()))
+            .get(MovieListViewModel::class.java)
+
         return inflater.inflate(R.layout.fragment_movie_list, container, false)
     }
 
@@ -23,10 +32,22 @@ class MovieListFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         setupRecyclerView()
+        setupListeners()
+        setupCall()
     }
 
     private fun setupRecyclerView() {
-        movies_list_recycler_view.adapter = MediaListAdapter()
+        movies_list_recycler_view.adapter = adapter
         movies_list_recycler_view.layoutManager = LinearLayoutManager(requireContext())
+    }
+
+    private fun setupListeners() {
+        movieListViewModel.getMovieList().observe(this, Observer {
+            adapter.loadItems(it ?: emptyList())
+        })
+    }
+
+    private fun setupCall() {
+        movieListViewModel.fetchPopularMovieList()
     }
 }
