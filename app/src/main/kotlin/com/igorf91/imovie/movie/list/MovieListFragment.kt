95d5de1c237184c8d51b5b2ec5d549f044caeb7d
igor <igor.fds91@hotmail.com>
2020-11-03 01:33:07 -0300

    update project

--- app/src/main/kotlin/com/igorf91/imovie/movie/list/MovieListFragment.kt
+++ app/src/main/kotlin/com/igorf91/imovie/movie/list/MovieListFragment.kt
@@ -1,35 +1,25 @@
 package com.igorf91.imovie.movie.list
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.igorf91.imovie.R
 import com.igorf91.imovie.adapter.MediaListAdapter
 import kotlinx.android.synthetic.main.fragment_movie_list.movies_list_recycler_view
 
-class MovieListFragment : Fragment() {
+class MovieListFragment : Fragment(R.layout.fragment_movie_list) {
 
     private val adapter = MediaListAdapter()
     private lateinit var movieListViewModel: MovieListViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        movieListViewModel = ViewModelProviders
-            .of(this, MovieListViewModelFactory(MovieListRepository()))
-            .get(MovieListViewModel::class.java)
-
-        return inflater.inflate(R.layout.fragment_movie_list, container, false)
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        movieListViewModel = ViewModelProvider(this, MovieListViewModelFactory(MovieListRepository()))
+            .get(MovieListViewModel::class.java)
 
         setupRecyclerView()
         setupListeners()
@@ -42,7 +32,7 @@ class MovieListFragment : Fragment() {
     }
 
     private fun setupListeners() {
-        movieListViewModel.getMovieList().observe(this, Observer {
+        movieListViewModel.getMovieList().observe(viewLifecycleOwner, Observer {
             adapter.loadItems(it ?: emptyList())
         })
     }
